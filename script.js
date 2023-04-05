322faded25091a62332abbf189ff8005a5bc53fb
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-04-05 13:58:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
-
+var btn = document.getElementById('swap');
+function swapTheme(){
+	var application = document.getElementById('app');
+	application.classList.add('night');
+	btn.classList.add('button_night');
+}
+btn.addEventListener('click',swapTheme);
